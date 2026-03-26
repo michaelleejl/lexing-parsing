90e90ae0d4587a937d56c141ab30787ccc029629
michaelleejl <michaelleejl@gmail.com>
2026-03-26 20:18:10 +0000

    Poke holes in the implementation

--- lib/lexing/combinators.ml
+++ lib/lexing/combinators.ml
@@ -6,36 +6,6 @@ module Recogniser = struct
   type r = Regex.t
   type t = char list -> char list outcome
 
-  let one_of cs = function
-    | [] -> Failure
-    | x :: xs -> if C.mem x cs then Success xs else Failure
-
-  let emp _ = Failure
-  let eps cs = Success cs
-  let seq r1 r2 cs = match r1 cs with Failure -> Failure | Success cs -> r2 cs
-
-  let alt r1 r2 cs =
-    match (r1 cs, r2 cs) with
-    | Failure, Failure -> Failure
-    | Success xs, Failure -> Success xs
-    | Failure, Success ys -> Success ys
-    | Success xs, Success ys ->
-        if List.length xs <= List.length ys then Success xs else Success ys
-
-  let rec kleene r cs =
-    match r cs with Failure -> Success cs | Success xs -> kleene r xs
-
-  let rec interpret r =
-    match r with
-    | Empty -> emp
-    | Epsilon -> eps
-    | Char cs -> one_of cs
-    | Alt (r1, r2) -> alt (interpret r1) (interpret r2)
-    | Seq (r1, r2) -> seq (interpret r1) (interpret r2)
-    | Kleene r -> kleene (interpret r)
-
-  let recognise r s =
-    match r (Base.String.to_list s) with Success [] -> true | _ -> false
 end
 
 module Lexer (Lang : L) = struct
@@ -48,72 +18,6 @@ module Lexer (Lang : L) = struct
   type t = lex_state -> lex_state outcome
 
   exception LexFailure
-
-  let one_of cs = function
-    | [] -> Failure
-    | x :: xs ->
-        if Regex.C.mem x cs then Success { matched = [ x ]; rest = xs }
-        else Failure
-
-  let eps s = Success { matched = []; rest = s }
-  let emp _ = Failure
-
-  let seq m1 m2 cs =
-    match m1 cs with
-    | Failure -> Failure
-    | Success { matched = matched1; rest } -> (
-        match m2 rest with
-        | Failure -> Failure
-        | Success { matched = matched2; rest } ->
-            Success { matched = matched1 @ matched2; rest })
-
-  let alt (m1 : s) m2 cs =
-    match (m1 cs, m2 cs) with
-    | Failure, Failure -> Failure
-    | Success s, Failure -> Success s
-    | Failure, Success s' -> Success s'
-    | Success s, Success s' ->
-        if List.length s.rest <= List.length s'.rest then Success s
-        else Success s'
-
-  let kleene m cs =
-    let rec kleene' m cs =
-      match m cs with
-      | Failure -> { matched = []; rest = cs }
-      | Success { matched; rest } ->
-          let { matched = matched'; rest = rest' } = kleene' m rest in
-          { matched = matched @ matched'; rest = rest' }
-    in
-    Success (kleene' m cs)
-
-  let rec interpret' r =
-    match r with
-    | Empty -> emp
-    | Epsilon -> eps
-    | Char cs -> one_of cs
-    | Alt (r1, r2) -> alt (interpret' r1) (interpret' r2)
-    | Seq (r1, r2) -> seq (interpret' r1) (interpret' r2)
-    | Kleene r -> kleene (interpret' r)
-
-  let interpret r to_token { lexed; rest } =
-    let m = interpret' r in
-    match m rest with
-    | Failure -> Failure
-    | Success { matched; rest } -> (
-        match to_token matched with
-        | None -> Success { lexed; rest }
-        | Some t -> Success { lexed = t :: lexed; rest })
-
-  let alt_l l1 l2 s =
-    match (l1 s, l2 s) with
-    | Failure, Failure -> Failure
-    | Success s, Failure -> Success s
-    | Failure, Success s' -> Success s'
-    | Success s, Success s' ->
-        if List.length s.rest <= List.length s'.rest then Success s
-        else Success s'
-
-  let ( >>| ) = alt_l
 
   let lex_step l state =
     match l state with
--- lib/lexing/generators.ml
+++ lib/lexing/generators.ml
@@ -1,14 +1,5 @@
 open Regex
 
-let rec compile_regex r =
-  match r with
-  | Empty -> Nfa.empty
-  | Epsilon -> Nfa.epsilon
-  | Char cs -> Nfa.one_of (C.to_list cs)
-  | Alt (r1, r2) -> Nfa.alt (compile_regex r1) (compile_regex r2)
-  | Seq (r1, r2) -> Nfa.seq (compile_regex r1) (compile_regex r2)
-  | Kleene r -> Nfa.kleene (compile_regex r)
-
 module Recogniser = struct
   type r = Regex.t
   type t = Dfa.t
@@ -46,65 +37,6 @@ module Lexer (Lang : L) (Tag : T with type token = Lang.token) = struct
     buffer : char list;
     last_accepting : (int * state) option;
   }
-
-  let lex_step machine { state; rest; tokens; buffer; last_accepting } =
-    if is_rejecting machine state then
-      match last_accepting with
-      | None -> raise (LexFailure "no last accepting state")
-      | Some (k, qs) -> (
-          let tag = emit_tag machine qs in
-          match tag with
-          | None -> raise (LexFailure "tag is empty")
-          | Some tag -> (
-              let chars = List.drop k buffer in
-              let buffer = List.take k buffer in
-              let action = (Tag.tag_to_action tag) (List.rev chars) in
-              let last_accepting = None in
-              let state = initialise machine in
-              let rest = List.rev buffer @ rest in
-              let buffer = [] in
-              match action with
-              | None -> { rest; tokens; buffer; last_accepting; state }
-              | Some t ->
-                  { rest; tokens = t :: tokens; buffer; last_accepting; state })
-          )
-    else
-      match rest with
-      | [] ->
-          if is_accepting machine state then
-            let tag = emit_tag machine state in
-            match tag with
-            | None -> raise (LexFailure "tag is empty")
-            | Some tag -> (
-                let action = (Tag.tag_to_action tag) (List.rev buffer) in
-                match action with
-                | None ->
-                    { rest; tokens; buffer = []; last_accepting = None; state }
-                | Some t ->
-                    {
-                      rest;
-                      tokens = t :: tokens;
-                      buffer = [];
-                      last_accepting = None;
-                      state;
-                    })
-          else raise (LexFailure "last state was not accepting")
-      | c :: rest ->
-          let next_state = step machine state c in
-          let new_accepting =
-            if is_accepting machine next_state then Some (0, next_state)
-            else
-              match last_accepting with
-              | None -> None
-              | Some (i, qs) -> Some (i + 1, qs)
-          in
-          {
-            rest;
-            tokens;
-            buffer = c :: buffer;
-            last_accepting = new_accepting;
-            state = next_state;
-          }
 
   let rec lex_run machine state =
     match (state.rest, state.buffer) with
