3abb930d47e727959ddda662e0ea5e608a609249
Michael Lee <michael@vbr.sh>
2026-08-05 10:14:27 +0100

    add tests for LL1 parser combinator

---
+++ test/test_parse_combinator_ll1.ml
@@ -0,0 +1,25 @@
+open Printf
+open Lexparse.Mlot
+open Lexparse.Parsing.Combinators.LL1(Grammars.LeftFactored)
+
+let print_ast x = Ast.to_str x |> printf "%s"
+
+let%expect_test _ =
+  print_ast (parse [ FUN; IDENT "x"; ARROW; IDENT "x"; PLUS; NUM 0 ]);
+  [%expect {| Fun(x, Plus(x, 0)) |}]
+
+let%expect_test _ =
+  print_ast (parse [ NUM 1; PLUS; NUM 2; PLUS; NUM 3 ]);
+  [%expect {| Plus(Plus(1, 2), 3) |}]
+
+let%expect_test _ =
+  print_ast (parse [ NUM 1; PLUS; LPAREN; NUM 2; PLUS; NUM 3; RPAREN ]);
+  [%expect {| Plus(1, Plus(2, 3)) |}]
+
+let%expect_test _ =
+  print_ast (parse [ NUM 1; PLUS; NUM 2; EQUALS; NUM 3 ]);
+  [%expect {| Equals(Plus(1, 2), 3) |}]
+
+let%expect_test _ =
+  print_ast (parse [ Token.IDENT "f"; LPAREN; NUM 1; PLUS; NUM 2; RPAREN ]);
+  [%expect {| App(f, Plus(1, 2)) |}]
