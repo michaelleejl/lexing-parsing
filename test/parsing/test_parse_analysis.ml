f262c129560cd9097ead4304c71422bc14a37f13
Michael Lee <michael@vbr.sh>
2026-08-14 14:34:20 +0100

    update test

--- test/parsing/test_parse_analysis.ml
+++ test/parsing/test_parse_analysis.ml
@@ -4,13 +4,17 @@ open Lexparse
 open Lexparse.Mlot
 open Lexparse.Parsing.Analysis
 
+(* Reports on the elaborated grammar, so the augmented production [S' ::= E $]
+   shows up alongside the grammar's own. *)
 module Report (Grammar : GRAMMAR) = struct
   module Elaborated = Elaborate (Grammar)
-  module A = GrammarAnalysis (Elaborated.Bnf)
+  module Bnf = Elaborated.Bnf
+  module A = GrammarAnalysis (Bnf)
+  open Views (Bnf)
 
-  let nt = Grammar.Nonterminal.to_string
-  let term = Grammar.Terminal.to_string
-  let sym = Grammar.Sym.to_string
+  let nt = Bnf.Nonterminal.to_string
+  let term = Bnf.Terminal.to_string
+  let sym = Bnf.Sym.to_string
   let te = function A.TE.Eps -> "eps" | A.TE.Term t -> term t
   let tset s = A.TSet.elements s |> List.map term |> String.concat " "
   let teset s = A.TESet.elements s |> List.map te |> String.concat " "
@@ -19,16 +23,11 @@ module Report (Grammar : GRAMMAR) = struct
     | [] -> "eps"
     | rhs -> List.map sym rhs |> String.concat " "
 
-  let productions =
-    let rec group = function
-      | [] -> []
-      | (p : Grammar.production) :: ps ->
-          let same, rest =
-            List.partition (fun (q : Grammar.production) -> q.lhs = p.lhs) ps
-          in
-          (p.lhs, p :: same) :: group rest
-    in
-    group (Grammar.productions.start :: Grammar.productions.rest)
+  let productions = production_rules
+
+  (* Lift a symbol of the source grammar into the elaborated one. *)
+  let t x : Bnf.sym = Bnf.T x
+  let n x : Bnf.sym = Bnf.N (Bnf.Nonterminal.Source x)
 
   let nullable () =
     A.NTSet.elements A.Nullable.set
@@ -44,7 +43,7 @@ module Report (Grammar : GRAMMAR) = struct
       (fun n ts -> printf "follow(%-5s) = %s\n" (nt n) (tset ts))
       A.Follow.table
 
-  let predict_set lhs (p : Grammar.production) =
+  let predict_set lhs (p : Bnf.production) =
     let first = A.First.syms p.rhs |> A.drop_eps in
     if A.Nullable.syms p.rhs then A.TSet.union first (A.Follow.nonterminal lhs)
     else first
@@ -53,7 +52,7 @@ module Report (Grammar : GRAMMAR) = struct
     List.iter
       (fun (lhs, rhss) ->
         List.iter
-          (fun (p : Grammar.production) ->
+          (fun (p : Bnf.production) ->
             printf "%-5s ::= %-24s { %s }\n" (nt lhs) (rhs_str p.rhs)
               (tset (predict_set lhs p)))
           rhss)
@@ -97,7 +96,7 @@ let%expect_test "left-factored: first" =
   Factored.first ();
   [%expect
     {|
-    first(Start) = IDENT NUM TRUE FALSE FUN LPAREN LET
+    first(S'   ) = IDENT NUM TRUE FALSE FUN LPAREN LET
     first(E    ) = IDENT NUM TRUE FALSE FUN LPAREN LET
     first(E'   ) = IDENT REC
     first(T'   ) = EQUALS eps
@@ -113,7 +112,7 @@ let%expect_test "left-factored: follow" =
   Factored.follow ();
   [%expect
     {|
-    follow(Start) =
+    follow(S'   ) =
     follow(E    ) = RPAREN IN EOF
     follow(E'   ) = RPAREN IN EOF
     follow(T'   ) = RPAREN IN EOF
@@ -129,21 +128,21 @@ let%expect_test "left-factored: predict sets" =
   Factored.predict ();
   [%expect
     {|
-    Start ::= E EOF                    { IDENT NUM TRUE FALSE FUN LPAREN LET }
+    S'    ::= E EOF                    { IDENT NUM TRUE FALSE FUN LPAREN LET }
     E     ::= FUN IDENT ARROW E        { FUN }
     E     ::= LET E'                   { LET }
     E     ::= T                        { IDENT NUM TRUE FALSE LPAREN }
     E'    ::= IDENT EQUALS E IN E      { IDENT }
     E'    ::= REC IDENT EQUALS E IN E  { REC }
-    T     ::= F T'                     { IDENT NUM TRUE FALSE LPAREN }
     T'    ::= EQUALS F T'              { EQUALS }
     T'    ::= eps                      { RPAREN IN EOF }
-    F     ::= G F'                     { IDENT NUM TRUE FALSE LPAREN }
+    T     ::= F T'                     { IDENT NUM TRUE FALSE LPAREN }
     F'    ::= PLUS G F'                { PLUS }
     F'    ::= eps                      { RPAREN EQUALS IN EOF }
-    G     ::= S G'                     { IDENT NUM TRUE FALSE LPAREN }
+    F     ::= G F'                     { IDENT NUM TRUE FALSE LPAREN }
     G'    ::= S G'                     { IDENT NUM TRUE FALSE LPAREN }
     G'    ::= eps                      { RPAREN PLUS EQUALS IN EOF }
+    G     ::= S G'                     { IDENT NUM TRUE FALSE LPAREN }
     S     ::= IDENT                    { IDENT }
     S     ::= NUM                      { NUM }
     S     ::= TRUE                     { TRUE }
@@ -163,7 +162,7 @@ let%expect_test "general: first" =
   Unfactored.first ();
   [%expect
     {|
-    first(Start) = IDENT NUM TRUE FALSE FUN LPAREN LET
+    first(S'   ) = IDENT NUM TRUE FALSE FUN LPAREN LET
     first(E    ) = IDENT NUM TRUE FALSE FUN LPAREN LET
     first(T'   ) = EQUALS eps
     first(T    ) = IDENT NUM TRUE FALSE LPAREN
@@ -178,7 +177,7 @@ let%expect_test "general: follow" =
   Unfactored.follow ();
   [%expect
     {|
-    follow(Start) =
+    follow(S'   ) =
     follow(E    ) = RPAREN IN EOF
     follow(T'   ) = RPAREN IN EOF
     follow(T    ) = RPAREN IN EOF
@@ -193,20 +192,20 @@ let%expect_test "general: predict sets" =
   Unfactored.predict ();
   [%expect
     {|
-    Start ::= E EOF                    { IDENT NUM TRUE FALSE FUN LPAREN LET }
+    S'    ::= E EOF                    { IDENT NUM TRUE FALSE FUN LPAREN LET }
     E     ::= FUN IDENT ARROW E        { FUN }
     E     ::= LET IDENT EQUALS E IN E  { LET }
     E     ::= LET REC IDENT EQUALS E IN E { LET }
     E     ::= T                        { IDENT NUM TRUE FALSE LPAREN }
-    T     ::= F T'                     { IDENT NUM TRUE FALSE LPAREN }
     T'    ::= EQUALS F T'              { EQUALS }
     T'    ::= eps                      { RPAREN IN EOF }
-    F     ::= G F'                     { IDENT NUM TRUE FALSE LPAREN }
+    T     ::= F T'                     { IDENT NUM TRUE FALSE LPAREN }
     F'    ::= PLUS G F'                { PLUS }
     F'    ::= eps                      { RPAREN EQUALS IN EOF }
-    G     ::= S G'                     { IDENT NUM TRUE FALSE LPAREN }
+    F     ::= G F'                     { IDENT NUM TRUE FALSE LPAREN }
     G'    ::= S G'                     { IDENT NUM TRUE FALSE LPAREN }
     G'    ::= eps                      { RPAREN PLUS EQUALS IN EOF }
+    G     ::= S G'                     { IDENT NUM TRUE FALSE LPAREN }
     S     ::= IDENT                    { IDENT }
     S     ::= NUM                      { NUM }
     S     ::= TRUE                     { TRUE }
@@ -231,15 +230,15 @@ let%expect_test "first of eps" =
   [%expect {| eps |}]
 
 let%expect_test "first stops at the first non-nullable symbol" =
-  show_first [ F.T PLUS; F.N E ];
+  show_first [ Factored.t PLUS; Factored.n F.Nonterminal.E ];
   [%expect {| PLUS |}]
 
 let%expect_test "first sees through a nullable prefix" =
-  show_first [ F.N G'; F.T EQUALS ];
+  show_first [ Factored.n F.Nonterminal.G'; Factored.t EQUALS ];
   [%expect {| IDENT NUM TRUE FALSE LPAREN EQUALS |}]
 
 let%expect_test "first of an all-nullable sequence keeps eps" =
-  show_first [ F.N G'; F.N T' ];
+  show_first [ Factored.n F.Nonterminal.G'; Factored.n F.Nonterminal.T' ];
   [%expect {| IDENT NUM TRUE FALSE LPAREN EQUALS eps |}]
 
 let%expect_test "nullable: empty sequence" =
@@ -247,9 +246,9 @@ let%expect_test "nullable: empty sequence" =
   [%expect {| true |}]
 
 let%expect_test "nullable: all-nullable sequence" =
-  show_nullable [ F.N G'; F.N T' ];
+  show_nullable [ Factored.n F.Nonterminal.G'; Factored.n F.Nonterminal.T' ];
   [%expect {| true |}]
 
 let%expect_test "nullable: sequence with a terminal" =
-  show_nullable [ F.N G'; F.T PLUS ];
+  show_nullable [ Factored.n F.Nonterminal.G'; Factored.t PLUS ];
   [%expect {| false |}]
