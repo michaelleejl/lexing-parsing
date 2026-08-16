1ec73bf44c5c1dc08f1612c340bb23b3c8571a1a
Michael Lee <michaelleejl@gmail.com>
2026-08-16 22:53:07 +0100

    add slr1

--- lib/parsing/bottomup/combinators.mli
+++ lib/parsing/bottomup/combinators.mli
@@ -6,3 +6,11 @@ module General (Gram : GRAMMAR) : sig
 
   val parse : token list -> ast
 end
+
+module SLR1 (Gram : GRAMMAR) : sig
+  type token = Gram.token
+  type ast = Gram.ast
+
+  val parse : token list -> ast
+end
+
